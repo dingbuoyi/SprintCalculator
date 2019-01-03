7875445045f765e0d36ab8ef9275169ca48be4cf
Dean <dean@thenetcircle.com>
2019-01-03 23:47:21 +0800

    add layout

--- app/src/main/java/com/dingbuoyi/sprintcalculator/MainActivity.kt
+++ app/src/main/java/com/dingbuoyi/sprintcalculator/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.dingbuoyi.sprintcalculator
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import java.util.*
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,9 +19,33 @@ class MainActivity : AppCompatActivity() {
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+                .setAction("Action", null).show()
+        }
+
+        startDateBtn.setOnClickListener {
+            selectStartDate()
         }
     }
+
+    private fun selectStartDate() {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+        val minute = calendar.get(Calendar.MINUTE)
+
+        val startDate = StringBuffer()
+        //实例化DatePickerDialog对象
+        var datePickerDialog =
+            DatePickerDialog(this@MainActivity, DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                //因为monthOfYear会比实际月份少一月所以这边要加1
+                startDate.append(year).append("/").append(month + 1).append("/").append(dayOfMonth)
+                startDateTextView.text = startDate
+            }, year, month, day)
+        datePickerDialog.show()
+    }
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -30,7 +57,7 @@ class MainActivity : AppCompatActivity() {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        return when(item.itemId) {
+        return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
